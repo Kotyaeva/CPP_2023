3b809f0fe8309d19fe27eec5782ed0661a5fc7e3
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-11 18:26:55 +0800

    Create S31.cpp

---
+++ S31.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+
+
+    string m[10] = {"ноль","Один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+    int number;
+    cout<<" введите число от 0 до 9: ";
+    cin>>number;
+    if ((number<0)||(number>9))
+    {
+        cout<<"Данное число не входит в диапазон от 0 до 9!!!"<<endl;
+    }
+    int len=sizeof(m)/sizeof(m[0]);
+    for(int i=0; i<=len; ++i)
+    {
+        if (i==number)
+        {
+            cout<<m[i]<<endl;
+            return 0;
+        }
+        }
+    }
+
