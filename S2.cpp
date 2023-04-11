6f6aa8181c53ae76f3da86805807e7a2f0d96ca2
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-11 18:31:05 +0800

    Create S2.cpp

---
+++ S2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    a=13;
+    b=5;
+    cout << "деление a и b:" <<a / b<< endl;
+cout<<"остаток от целочисленного деления:"<<a%b;
+    return 0;
+}
