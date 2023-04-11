6ac2bdf1250eb1860d08cfc630672185bba5199a
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-11 18:37:55 +0800

    Create M01.cpp

---
+++ M01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   for (int i=1; i<=9; ++i)
+   {
+       for (int o=1; o<=9; ++o)
+
+       {
+           cout << i*o << '\t';
+       }
+       cout<<endl;
+   }
+}
