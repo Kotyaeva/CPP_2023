49040ab72a2dd92fee07fa04b7b0688cc04e74ec
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-23 23:49:46 +0800

    Update S21.cpp

--- S21.cpp
+++ S21.cpp
@@ -2,23 +2,24 @@
 #include <fstream>
 using namespace std;
 
-    int main() {
+int main(){
     setlocale(LC_ALL, "Russian");
     int A,B;
     cout<<"Число A: ";
     cin>> A;
     cout<<(" Число B ( больше числа A): ");
     cin>> B;
-    if (B<A)
-        {
-        cout<<"Число A больше числа B"<<endl;
-        }
+    if (B<A){
+
+    cout<<"Число A больше числа B"<<endl;
+}
+
     ofstream out;
     out.open("output.txt");
-    for (int i=A; i<=B; ++i)
-        {
+    for (int i=A; i<=B; ++i){
     out<<i*3<<endl;
-        }
-    return 0;
-    out.close();
-    }
+}
+
+return 0;
+out.close();
+}
