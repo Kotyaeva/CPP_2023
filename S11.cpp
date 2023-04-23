6ccb4d2d71dc4a47a98d1b117e0ce6753c4a85c9
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-23 23:42:58 +0800

    Update S11.cpp

--- S11.cpp
+++ S11.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
-int main()
- for(
-    int x = 0; x <= 100; x++)
+int main(){
+    
+    for(int x = 0; x <= 100; x++){
     if(x%3 == 0)
     cout << x << endl;
-{
+    
+}
     return 0;
 }
