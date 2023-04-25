2a99661ca8a5bda47344f4ee0895aa51731c8b83
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-25 20:41:08 +0800

    Create S22.cpp

---
+++ S22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+ int main(){
+    ifstream file("output.txt");
+    int n=1, b;
+       while(file>>b){
+	   cout<<n<<". "<<b<<endl;
+	   ++n;}
+    file.close();
+    return 0;
+
+}
