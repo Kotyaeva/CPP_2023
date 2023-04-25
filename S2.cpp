89a7dacfebe06cf501a4244585396b6934e9e9bb
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-25 20:21:44 +0800

    Update S2.cpp

--- S2.cpp
+++ S2.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main(){
-    
     setlocale(LC_ALL, "Russian");
     int a, b;
-    a=13;
-    b=5;
+        a=13;
+        b=5;
     cout << "деление a и b:" <<a / b<< endl;
     cout<<"остаток от целочисленного деления:"<<a%b;
-    
 return 0;
 }
