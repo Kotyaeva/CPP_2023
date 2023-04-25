5d889240ececdf450e0b8e9d4134e14cb52d206b
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-04-25 20:13:52 +0800

    Create S12.cpp

---
+++ S12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    int a=1, b=0;
+        while(a<=1000){
+ a=a+4;
+                if(a<=1000)
+                if(a%7==0){
+ b=b+a;}
+                else{
+ b=b;}
+        }
+ cout<<"Сумма чисел равна: "<<b<<endl;
+    return 0;
+}
