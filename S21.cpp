364987a8df02fe761226e35d9ef4e100a89f31c6
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-05-03 14:03:17 +0800

    Update S21.cpp

--- S21.cpp
+++ S21.cpp
@@ -9,14 +9,11 @@ int main(){
     cin>> A;
     cout<<(" Число B ( больше числа A): ");
     cin>> B;
-        if (B<A){
-            cout<<"Число A больше числа B"<<endl;
-}
-    ofstream out;
-    out.open("output.txt");
-        for (int i=A; i<=B; ++i){
-            out<<i*3<<endl;
-}
+    if (B<A){
+        cout<<"Число A больше числа B"<<endl;
+        }
+    ofstream out;out.open("output.txt");
+    for (int i=A; i<=B; ++i){out<<i*3<<endl;}
     return 0;
     out.close();
-}
+    }
