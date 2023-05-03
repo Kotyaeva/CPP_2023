6c6b756e3352c80e03afdd4c84a85b819a581200
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-05-03 13:45:26 +0800

    Update S11.cpp

--- S11.cpp
+++ S11.cpp
@@ -5,13 +5,11 @@ int main(){
     setlocale(LC_ALL, "Russian");
     int a=1, s=0;
     while(a<=1000){
-        a=a+4;
-            if(a<=1000)
-            if(a%7==0){
-                s=s+a;}
-            else{
-                s=s;}
-}
+            a=a+4;
+    if(a<=1000)
+        if(a%7==0){s=s+a;}
+    else{s=s;}
+    }
     cout<<"Сумма чисел равна: "<<s<<endl;
     return 0;
-}
+    }
