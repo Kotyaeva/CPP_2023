243e345021bfcc70901491db43780e1bad10dc79
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-05-03 13:42:40 +0800

    Update M01.cpp

--- M01.cpp
+++ M01.cpp
@@ -3,13 +3,10 @@
 using namespace std;
 
 int main(){
-    
     for (int i=1; i<=9; ++i){
-    for (int o=1; o<=9; ++o){
-    cout << i*o << '\t';
-}
-
+            for (int o=1; o<=9; ++o){
+                cout << i*o << '\t';
+                }
     cout<<endl;
-}
-}
-
+    }
+    };
