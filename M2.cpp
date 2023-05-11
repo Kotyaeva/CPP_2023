022861aae3adc10d83ab963d69005ef285cc1814
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-05-11 22:43:35 +0800

    Update M2.cpp

--- M2.cpp
+++ M2.cpp
@@ -6,16 +6,14 @@ int pl(int B, int C){
     float  S=0;
     double const pi=3.14159265359;
     S=(B*B*C)/(4*(tan(pi/C)));
-    cout<<"Площадь равна "<<C<<"-угольника. Число сторон "<<B<<" равно "<<S<<endl;
+    cout<<"Площадь "<<C<<"-угольника. С количеством сторон "<<B<<" равна "<<S<<endl;
     return S;
 }
 
 int main(){
     setlocale(LC_ALL, "Russian");
     int A, N;
-    cout<<"Введите сторону A: ";
-    cin>>A;
-    cout<<"Введите число сторон N: ";
-    cin>>N;
+    A=10; // количество сторон
+    N=30; //количество углов
     pl(A, N);
 }
