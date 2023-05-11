868c66ec6492656029eebe3f151903b74106b738
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-05-11 22:49:46 +0800

    S51.cpp

---
+++ S51.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+setlocale(LC_ALL, "Russian");
+string a;
+cout << "Введите слово: ";
+cin >> a;
+for (int i = a.size() - 1; i >= 0; i--){
+        cout << a[i];
+}
+
+return 0;
+}
