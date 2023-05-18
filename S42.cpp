4ef97f00950131274e94307aa0b331e5537359e2
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-05-18 16:29:03 +0800

    Update S42.cpp

--- S42.cpp
+++ S42.cpp
@@ -9,23 +9,23 @@ sort(a);
 return 0;
 }
 
-
 void sort(int *massive){
 int number=0;
 int length=10;
-
 for (int i=0;i<length;i++){
         for (int j=1+i;j<length;j++){
-        if (massive[i] < massive[j]){
-            number = massive[i];
-            massive[i] = massive[j];
-            massive[j] = number;
+                if (massive[i] < massive[j]){
+                    number = massive[i];
+                    massive[i] = massive[j];
+                    massive[j] = number;
+                }
+        
+                else if (massive[i] == massive[j]){
+                    massive[i] = massive[length-1];
+                    length--;
+                }   
         }
-        else if (massive[i] == massive[j]) {
-            massive[i] = massive[length-1];
-            length--;
-        }
- }
+        
 cout << massive[i] << " ";
  }
  }
