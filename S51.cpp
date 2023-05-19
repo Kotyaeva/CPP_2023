825680a48467e879fe89ece38f1d6558aae02ce2
Kotyaeva <126486952+Kotyaeva@users.noreply.github.com>
2023-05-19 13:26:38 +0800

    Update S51.cpp

--- S51.cpp
+++ S51.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){
-setlocale(LC_ALL, "Russian");
-string a;
-cout << "Введите слово: ";
-cin >> a;
-for (int i = a.size() - 1; i >= 0; i--){
-cout << a[i];
-}
+    setlocale(LC_ALL, "Russian");
+    string a;
+    cout << "Введите слово: ";
+    cin >> a;
+    for (int i = a.size() - 1; i >= 0; i--){
+    cout << a[i];
+    }
 
 return 0;
 }
